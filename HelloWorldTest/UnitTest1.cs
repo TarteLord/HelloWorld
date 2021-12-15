14530642d66cde9f4faf458707747a2a621337b0
Lauge Korsholm <laug0040@elev.tec.dk>
2021-12-15 11:08:36 +0100

    true test2

--- HelloWorldTest/UnitTest1.cs
+++ HelloWorldTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace HelloWorldTest
 {
     public class UnitTest1
     {
-        private const string Expected = "Hello World!123";
+        private const string Expected = "Hello World!";
 
         [Fact]
         public void TestOutputIsHelloWorld()
